e89ee1bc7701627c648741d08f6d04b81982bb74
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-01-12 23:04:10 -0800

    Time: 2708 ms (5.02%), Space: 59.2 MB (35.36%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,14 +1,12 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         points.sort()
-        # print(points)
         count = 1
         curr_intersection = points[0]
         for i in range(1,len(points)):
             curr = points[i]
             if curr_intersection[0] <= curr[0] <= curr_intersection[1] :
                 curr_intersection = [max(curr[0], curr_intersection[0]), min(curr[1], curr_intersection[1])]   
-                # print(curr_intersection)
             else:
                 count += 1
                 curr_intersection = curr
