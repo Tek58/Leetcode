84694c24628534b439ec4313557f0a42bf6d933c
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-01-23 00:11:40 -0800

    Time: 47 ms (21.83%), Space: 13.9 MB (99.21%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -1,13 +1,12 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        out = []
-        queue = deque(range(1,10))
-        while queue:
-            elem = queue.popleft()
-            if low <= elem <= high:
-                out.append(elem)
-            last = elem % 10
-            if last < 9: 
-                queue.append(elem*10 + last + 1)
-                    
-        return out
+        answer=[]
+        for i in range(1,10):
+            number=i
+            next=i
+            while(number<=high and next<10):
+                if number>=low:
+                    answer.append(number)
+                next=next+1
+                number=number*10+next
+        return sorted(answer)
