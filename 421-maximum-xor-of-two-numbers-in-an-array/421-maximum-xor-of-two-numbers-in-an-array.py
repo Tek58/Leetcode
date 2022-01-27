d570445785adf7a02ee771a14fdcfb33f5cbd670
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-01-26 23:33:33 -0800

    Time: 912 ms (88.44%), Space: 33.4 MB (97.65%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.py
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.py
@@ -1,22 +1,8 @@
 class Solution:
     def findMaximumXOR(self, nums: List[int]) -> int:
-        numbers, root, result = [], [None, None], 0
-        for num in nums:
-            numbers.append([0] * 32)
-            for ind in range(1, 33):
-                numbers[-1][-ind] = num % 2
-                num = num >> 1
-                
-            node = root
-            for n in numbers[-1]:
-                if not node[n]:
-                    node[n] = [None, None]
-                node = node[n]
-        for num in numbers:
-            node, temp = root, 0
-            for n in num:
-                temp = (temp << 1) + (node[1 - n] is not None)
-                node = node[(1 - n) if node[1 - n] else n]
-            result = max(result, temp)
-        return result
-    
+        answer = 0
+        for i in range(32)[::-1]:
+            answer <<= 1
+            prefixes = {num >> i for num in nums}
+            answer += any(answer^1 ^ p in prefixes for p in prefixes)
+        return answer
