b799050c4858f39626cd218edac64b1d741ddb13
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-02-03 02:50:31 -0800

    Time: 604 ms (94.41%), Space: 14.2 MB (90.85%) - LeetHub

---
+++ 454-4sum-ii/454-4sum-ii.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        container = defaultdict(int)
+        counter = 0
+        for i in nums1:
+            for j in nums2:
+                container[i+j] += 1
+        
+        for j in nums3:
+            for k in nums4:
+                val = -(j+k)
+                if val in container:
+                    counter += container[val]
+
+        return counter
+        
