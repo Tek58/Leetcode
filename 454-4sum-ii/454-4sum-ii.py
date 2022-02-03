d50c57dd9f068e397ff6557e2400d20481300e42
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-02-03 02:51:54 -0800

    Time: 624 ms (92.33%), Space: 14.2 MB (98.30%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.py
+++ 454-4sum-ii/454-4sum-ii.py
@@ -12,6 +12,6 @@ class Solution:
                 val = -(j+k)
                 if val in container:
                     counter += container[val]
-
+                    
         return counter
         
