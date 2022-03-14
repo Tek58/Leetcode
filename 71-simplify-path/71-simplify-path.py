08a01b9467456ac781afd6768ce57a901a69b9fa
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-03-14 10:46:46 +0300

    Time: 45 ms (58.05%), Space: 13.9 MB (87.69%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -1,6 +1,8 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
+        print(path.split("/"))
         absolute = [i for i in path.split("/") if i != "" if i != "."]
+        print(absolute)
         canonical = []
         for i in absolute:
             if i == "..":
