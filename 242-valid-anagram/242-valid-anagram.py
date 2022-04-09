11a21446b901a56266e42c8556f9c25b8bd71bf2
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-04-09 10:47:09 +0300

    Time: 66 ms (57.08%), Space: 14.4 MB (97.35%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,5 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
         store = Counter(s)
         for i in t:
             if i not in store:
