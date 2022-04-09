bc586976d07b626784f07787c211a2f1b652a603
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-04-09 11:34:05 +0300

    Time: 1754 ms (18.88%), Space: 25 MB (42.03%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) == 1:
-            return 0
         maxProfit = 0
         buyingDayIndex = 0
-        sellingDayIndex = 1
-        while sellingDayIndex < len(prices):
+        for sellingDayIndex in range(len(prices)):
             if prices[sellingDayIndex] <= prices[buyingDayIndex]:
                 buyingDayIndex = sellingDayIndex
             else:
                 maxProfit = max(maxProfit, (prices[sellingDayIndex] - prices[buyingDayIndex]))
-            sellingDayIndex += 1
         return maxProfit            
