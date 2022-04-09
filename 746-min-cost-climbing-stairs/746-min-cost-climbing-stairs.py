73142669920dfc09ac32996f1959c577c373f398
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-04-09 14:57:57 +0300

    Time: 76 ms (61.14%), Space: 16.8 MB (5.37%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -6,7 +6,7 @@ class Solution:
     
         @cache
         def dp(i):
-            if i >= len(cost):
+            if i < 0:
                 return 0
-            return min(dp(i+1) + cost[i], dp(i+2) + cost[i])
-        return min(dp(0), dp(1))
+            return min(dp(i-1) + cost[i], dp(i-2) + cost[i])
+        return min(dp(len(cost) -1), dp(len(cost) -2))
