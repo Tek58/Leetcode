0015818f93210744f5a7099765600fdaa9da73e0
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-04-13 11:21:35 +0300

    Time: 35 ms (82.95%), Space: 13.8 MB (85.84%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -1,27 +1,23 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         matrix = [[0]*n for i in range(n)]
-        topLeft = curr = (0,0)
-        topRight = (0, n-1)
-        bottomLeft = (n-1, 0)
-        bottomRight = (n-1, n-1)
+        top, bottom = 0, n
         count = 1
         while count <= n*n:
-            for i in range(topLeft[1], topRight[1]+1):
-                matrix[topLeft[0]][i] = count
+            for i in range(top, bottom):
+                matrix[top][i] = count
                 count += 1
-            for i in range(topRight[0]+1, bottomRight[0]+1):
-                matrix[i][topRight[1]] = count
+            for i in range(top + 1, bottom):
+                matrix[i][bottom-1] = count
                 count += 1
-            for i in range(bottomRight[1] - 1, bottomLeft[1]-1, -1 ):
-                matrix[bottomRight[0]][i] = count
+            for i in range(bottom - 2, top - 1 , -1 ):
+                matrix[bottom-1][i] = count
                 count += 1
-            for i in range(bottomLeft[0] - 1, topLeft[0], -1):
-                matrix[i][bottomLeft[1]] = count
+            for i in range(bottom - 2, top, -1):
+                matrix[i][top] = count
                 count += 1
             
-            topLeft = curr = (topLeft[0]+1, topLeft[1]+1)
-            topRight= (topRight[0]+1, topRight[1]-1)
-            bottomRight = (bottomRight[0]-1, bottomRight[1]-1)
-            bottomLeft = (bottomLeft[0]-1, bottomLeft[1]+1)
+            top += 1
+            bottom -= 1
         return matrix
+    
