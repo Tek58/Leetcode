62b391d84d1c0540c950ed11f96af560bf3b7bd1
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-04-26 15:25:53 +0300

    Time: 105 ms (63.73%), Space: 13.9 MB (96.21%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,10 +1,9 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         s1Count = Counter(s1)
-        s2Count = Counter(s2[:len(s1)-1])
+        s2Count = Counter(s2[:len(s1)])
         left = 0
-        for right in range(len(s1)-1, len(s2)):
-            s2Count[s2[right]] += 1
+        for right in range(len(s1), len(s2)):
             flag = True
             for key, val in s1Count.items():
                 if (key in s2Count and s2Count[key] != val) or key not in s2Count:
@@ -12,6 +11,13 @@ class Solution:
                     break
             if flag:
                 return flag
+            s2Count[s2[right]] += 1
             s2Count[s2[left]] -= 1
             left += 1              
-        return False
+        flag = True
+        for key, val in s1Count.items():
+            if (key in s2Count and s2Count[key] != val) or key not in s2Count:
+                flag = False
+                break
+        if flag:
+            return flag
