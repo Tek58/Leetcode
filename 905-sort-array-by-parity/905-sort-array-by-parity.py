514b8b9eb54c3b32df96914b2d212c4c8fa6ddd9
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-05-02 09:37:29 +0300

    Time: 148 ms (14.74%), Space: 14.8 MB (17.91%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.py
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -1,10 +1,14 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        even = []
-        odd = []
+        res = [0] * len(nums)
+        even = 0
+        odd = len(nums)-1
         for i in nums:
+            # print(i, even, odd)
             if i % 2 == 0:
-                even.append(i)
+                res[even] = i
+                even += 1
             else:
-                odd.append(i)
-        return even + odd
+                res[odd] = i
+                odd -= 1
+        return res
