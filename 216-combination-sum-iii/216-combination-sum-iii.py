2e676c5922bc6cf485df2c085af2d58451e35113
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-05-10 17:17:57 +0300

    Time: 44 ms (51.32%), Space: 14 MB (29.87%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -3,14 +3,16 @@ class Solution:
         res = []
         def dfs(num, curr, total):
             if len(curr) == k and total == 0:
-                res.append(curr)
+                res.append(curr[:])
                 return
             
             if total < 0 or len(curr) > k:
                 return
             
             for i in range(num+1, 10):
-                dfs(i, curr + [i], total - i)
+                curr.append(i)
+                dfs(i, curr, total - i)
+                curr.pop()
                 
         dfs(0, [], n)
         return res
