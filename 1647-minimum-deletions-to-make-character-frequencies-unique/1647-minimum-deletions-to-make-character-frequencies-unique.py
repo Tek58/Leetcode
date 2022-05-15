10f0036b8f6571d3aa9836e0470d2123120f8e99
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-05-16 01:23:33 +0300

    Time: 134 ms (86.28%), Space: 14.8 MB (55.37%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        store = Counter(s).values()
+        visited = set()
+        result  = 0 
+        mx = float("-inf") 
+        for val in store:
+            while val and val in visited:
+                val -= 1
+                result += 1
+            if val:
+                visited.add(val)
+        return result 
+            
+            
