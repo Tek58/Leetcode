8b56dbbb602f43528672a6efc8eddf8f090bcc01
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-05-18 21:12:25 +0300

    Time: 2302 ms (90.86%), Space: 74.2 MB (70.06%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -0,0 +1,25 @@
+class Solution:
+    def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+        graph = defaultdict(list)
+
+        for conn in connections:
+            graph[conn[0]].append(conn[1])
+            graph[conn[1]].append(conn[0])
+
+        low = [0] * n
+        res = []
+
+        def dfs(node, parent, reachable):
+            low[node] = reachable
+            for neighbor in graph[node]:
+                if parent != neighbor:
+                    if not low[neighbor]:
+                        dfs(neighbor, node, reachable+1)
+                    if low[neighbor] > reachable:
+                        res.append([node,neighbor])
+                    else:
+                        low[node] = min(low[neighbor], low[node])
+                    
+        dfs(0, -1, 1)
+        
+        return res
