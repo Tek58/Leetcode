1a5f8d1b6b81dc912b8bfb1ed5f808ad3ed5249e
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-05-25 13:19:40 +0300

    Time: 3980 ms (5.08%), Space: 61.7 MB (67.23%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.py
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
         envelopes.sort(key=lambda x: (x[0], -x[1]))
-        # print(envelopes)
             
         res = []
         for envelop in envelopes:
@@ -16,5 +15,4 @@ class Solution:
             else:
                 res.append(envelop)
                 
-        # print(res)
         return len(res)
