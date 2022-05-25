6b10431a8763e2a42f0fed63cd6e057e99721b24
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-05-25 13:17:57 +0300

    Time: 3824 ms (5.08%), Space: 61.7 MB (67.23%) - LeetHub

---
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        # print(envelopes)
+            
+        res = []
+        for envelop in envelopes:
+            if res and res[-1][0] < envelop[0] and res[-1][1] < envelop[1]: 
+                res.append(envelop)
+                continue
+
+            for i in range(len(res)):
+                if res[i][1] >= envelop[1]: 
+                    res[i] = envelop
+                    break
+            else:
+                res.append(envelop)
+                
+        # print(res)
+        return len(res)
