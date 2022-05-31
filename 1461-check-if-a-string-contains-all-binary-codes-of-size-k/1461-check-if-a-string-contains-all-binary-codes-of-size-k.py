69cb9fe5506bdcfe07dd0978274193ba5c11c8fa
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-05-31 09:29:48 +0300

    Time: 1088 ms (10.17%), Space: 26.9 MB (56.19%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -2,9 +2,9 @@ class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
         res = 2**k
         visited = set()
-
         for i in range(k, len(s)+1):
             val = s[i-k:i]
+            print(val)
             if val not in visited:
                 visited.add(val)
                 res -= 1
