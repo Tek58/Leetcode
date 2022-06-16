3b8b074c233772ebf09d358c988d4db2e0bfa912
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-06-16 21:38:38 +0300

    Time: 2292 ms (26.83%), Space: 13.9 MB (90.68%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,13 +1,13 @@
 class Solution:
-    def __init__(self):
-        self.longest = ""
-        self.longestLen = 0
     def longestPalindrome(self, s: str) -> str:
+        longest = ""
+        longestLen = 0
         def isPalindrome(left, right):
+            nonlocal longest, longestLen
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                if (right - left + 1) > self.longestLen:
-                    self.longest = s[left:right+1]
-                    self.longestLen = right - left + 1
+                if (right - left + 1) > longestLen:
+                    longest = s[left:right+1]
+                    longestLen = right - left + 1
                 left -= 1
                 right += 1
                 
@@ -15,4 +15,4 @@ class Solution:
             isPalindrome(i, i)
             isPalindrome(i, i + 1)
             
-        return self.longest
+        return longest
