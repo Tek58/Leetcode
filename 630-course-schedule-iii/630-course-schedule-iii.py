235f4e44e3cb8e361602e40408640403219a9ce3
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-06-23 23:24:52 +0300

    Time: 1515 ms (9.63%), Space: 18.2 MB (99.06%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+        courses.sort(key=lambda x:x[1])
+        print(courses)
+        heap = []
+        timeSofar = 0
+        for course in courses:
+            if timeSofar + course[0] <= course[1]:
+                heappush(heap, -course[0])
+                timeSofar += course[0]
+            elif heap:
+                top = -heappop(heap)
+                if top > course[0]:
+                    timeSofar -= top
+                    heappush(heap, -course[0])
+                    timeSofar += course[0]
+                else:
+                    heappush(heap, -top)
+        print(heap)
+        return len(heap)
+                
+            
