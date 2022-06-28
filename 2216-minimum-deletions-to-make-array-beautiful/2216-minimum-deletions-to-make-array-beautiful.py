54d81ae5516ae4b2ae4d785cd13ea9df42655c22
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-06-28 23:58:21 +0300

    Time: 1801 ms (66.59%), Space: 28.5 MB (42.77%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.py
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.py
@@ -1,8 +1,9 @@
 class Solution:
     def minDeletion(self, nums: List[int]) -> int:
+        n = len(nums)
         count = 0
-        for i in range(len(nums)):
+        for i in range(n):
             if (i + count) % 2 == 0:
-                if i + 1 < len(nums) and nums[i] == nums[i+1]:
+                if i + 1 < n and nums[i] == nums[i+1]:
                     count += 1
-        return count + 1 if (len(nums) - count) % 2 == 1 else count 
+        return count + 1 if (n - count) % 2 == 1 else count 
