482d6898a83a674dfbb4a94d38e81a21fc6370aa
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-07-08 22:34:39 +0300

    Time: 1164 ms (72.37%), Space: 44.8 MB (21.74%) - LeetHub

---
+++ 1473-paint-house-iii/1473-paint-house-iii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minCost(self, houses: List[int], cost: List[List[int]], m: int, n: int, target: int) -> int:
+        @lru_cache(None)
+        def dp(idx, prev_color, k):
+            if idx >= m:
+                if k == 0:
+                    return 0
+                return -1
+                    
+            if k < 0:
+                return -1
+
+            if houses[idx] != 0:
+                curr = 0 if prev_color == houses[idx] else 1
+                return dp(idx + 1, houses[idx], k - curr)
+
+            best = -1
+            for new_color in range(1, n + 1):
+                new_color_cost = cost[idx][new_color - 1]
+                val = 0 if prev_color == new_color else 1
+                other_costs = dp(idx + 1, new_color, k - val)
+
+                if other_costs >= 0:
+                    best = min(best if best > 0 else float("inf"), new_color_cost + other_costs)
+            return best
+
+
+        return dp(0, 0, target)
