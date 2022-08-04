d5a263d5d458323fba4aada99160d0f70b296831
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-08-04 11:07:41 +0300

    Time: 36 ms (86.14%), Space: 13.8 MB (63.37%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.py
+++ 858-mirror-reflection/858-mirror-reflection.py
@@ -1,16 +1,4 @@
 class Solution:
     def mirrorReflection(self, p: int, q: int) -> int:
-        extention = q 
-        reflection = p
-        while extention % 2 == 0 and reflection % 2 == 0:
-            extention //= 2
-            reflection //= 2
-        
-        if extention % 2 == 0 and reflection % 2 != 0:
-            return 0
-        if extention % 2 != 0 and reflection % 2 == 0:
-            return 2
-        if extention % 2 != 0 and reflection % 2 != 0:
-            return 1
-        
-        return -1
+        return ((p & -p) >= (q & -q)) + ((p & -p) > (q & -q))
+            
