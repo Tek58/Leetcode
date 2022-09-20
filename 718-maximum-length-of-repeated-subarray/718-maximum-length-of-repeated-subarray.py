2203e6a4e3871670a09d404bd02459cef6f177ae
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-09-20 20:41:32 +0300

    Time: 296 ms (96.25%), Space: 14 MB (94.69%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -1,16 +1,13 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        n = len(nums1)
-        m = len(nums2)
-        dp = [[0]*(n+1) for i in range(m+1) ]
-        
-        counter = 0
-        for i in range(1, m+1):
-            for j in range(1, n+1):
-                if nums2[i-1] == nums1[j-1]:
-                    diagonal = dp[i-1][j-1]
-                    dp[i][j] += (diagonal + 1)
-                    counter = max(counter, dp[i][j])
-                    
-        return counter
+        strnum2 = ''.join([chr(x) for x in nums2])
+        strmax = ''
+        res = 0
+        for num in nums1:
+            strmax += chr(num)
+            if strmax in strnum2:
+                res = max(res, len(strmax))
+            else:
+                strmax = strmax[1: ]
+        return res
                 
