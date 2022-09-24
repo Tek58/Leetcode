94dbbb0a81a1c9903b530ef3438c1d08bd47bab4
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-09-24 20:57:16 +0300

    Time: 643 ms (32.96%), Space: 15.2 MB (81.65%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        numPaths = 0
+        
+        def dfs(node, pathSums):
+            nonlocal numPaths
+            if node is None:
+                return
+            
+            lastSum = pathSums[-1] 
+            currSum = lastSum + node.val
+
+            for prevSum in pathSums:
+                diff = currSum - prevSum
+                if diff == targetSum:
+                    numPaths += 1
+            
+            pathSums.append(currSum)
+            
+            dfs(node.left, pathSums)
+            if node.left:
+                pathSums.pop()
+                
+            dfs(node.right, pathSums)
+            if node.right:
+                pathSums.pop()
+            
+        dfs(root, [0])
+        return numPaths
