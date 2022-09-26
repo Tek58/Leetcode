260ee6c2262664fbe9a0c443ed870021018aabad
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-09-26 10:22:50 +0300

    Time: 105 ms (11.95%), Space: 14.2 MB (9.51%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -1,25 +1,15 @@
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
         leader = [i for i in range(26)]
-        rank = [1]*26
-        
         def find(x):
             if x == leader[x]:
                 return x
-            leader[x] = find(leader[x])
-            return leader[x]
+            return find(leader[x])
         
         def union(x, y):
             xLeader = find(x)
             yLeader = find(y)
-            xCount = rank[x]
-            yCount = rank[y]
-            if xCount > yCount:
-                leader[xLeader] = yLeader
-                rank[x] += rank[y]
-            else:
-                leader[yLeader] = xLeader
-                rank[y] += rank[x]
+            leader[xLeader] = yLeader
             
         for equation in equations:
             x , y = equation[0], equation[-1]
