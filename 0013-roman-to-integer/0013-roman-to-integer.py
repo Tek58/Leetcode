d7fe4ed98cb657d2fb618a177fdd4ca0bccf32d0
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-10-23 14:50:50 +0300

    Time: 57 ms (85.33%), Space: 13.8 MB (76.91%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,22 +1,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        n = len(s)
-        romanNumbers = {"O": 0, "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000 }
-        specialNumbers = { "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900 }
-        s += "O"
-        index = 0
+        romanNumbers = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000 }
         res = 0
-        while index < n:
-            first = s[index]
-            second =s[index + 1]
-            curr = first + second
-            if curr in specialNumbers:
-                res += specialNumbers[curr]
-                index += 1
+        for i in range(len(s)-1):
+            first = romanNumbers[s[i]]
+            second = romanNumbers[s[i + 1]]
+            if first < second:
+                res -= first
             else:
-                res += romanNumbers[first]
-            index += 1
-        return res
+                res += first
+            
+        return res + romanNumbers[s[-1]]
             
 
 # Time Complexity O(n)
