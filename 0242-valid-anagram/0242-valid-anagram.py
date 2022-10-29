f619fb4cef4909acce1ceef61fc7a1d572924482
Taklemariam Alazar <56020472+Tek58@users.noreply.github.com>
2022-10-29 19:27:15 +0300

    Time: 87 ms (62.10%), Space: 14.5 MB (34.99%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sCount = Counter(s)
+        tCount = Counter(t)
+        for key, value in tCount.items():
+            if sCount[key] < value:
+                return False
+        return len(s) == len(t)
